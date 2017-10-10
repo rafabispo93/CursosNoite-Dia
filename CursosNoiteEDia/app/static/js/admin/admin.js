1b0e19b6b5c95c3bb48679f8a3a059bc3b7c83c4
Marcos Antônio de Souza Silva <chuckymass@gmail.com>
2017-10-10 13:52:42 -0300

    Ajusta problema de não conseguir pegar os dados enviados via formulário

--- CursosNoiteEDia/app/static/js/admin/admin.js
+++ CursosNoiteEDia/app/static/js/admin/admin.js
@@ -1,27 +1,28 @@
-$(function() {
-  var submitCurso, getFormData;
-
-  $("#register-curso").on('click', submitCurso.bind(this));
-
-  function submitCurso (ev) {
-    var inputs = $("#curso-form :input"), object = {};
-
-    for (count = 0; count < inputs.length; count++) {
-      object[inputs[count].id] = inputs[count].value;
+/*jslint plusplus: true, nomen: true*/
+/*global G, L, $, Materialize, console, window */
+$(function () {
+    'use strict';
+    function sendData(url, object) {
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: JSON.stringify(object),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
     }
-    sendData('/adicionarCurso', object);
-  };
-  function sendData (url, object) {
-    $.ajax({
-              url: url,
-              data: object,
-              type: 'POST',
-              success: function(response) {
-                  console.log(response);
-              },
-              error: function(error) {
-                  console.log(error);
-              }
-    });
-  }
+    function submitCurso() {
+        var inputs = $("#curso-form :input"), object = {}, count;
+        for (count = 0; count < inputs.length; count++) {
+            object[inputs[count].id] = inputs[count].value;
+        }
+        sendData('/adicionarCurso', object);
+    }
+    $("#register-curso").on('click', submitCurso);
 });
--- CursosNoiteEDia/app/views.py
+++ CursosNoiteEDia/app/views.py
@@ -4,16 +4,21 @@ from flask import render_template, request
 
 from app import app
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/admin')
 def admin():
     return render_template("admin.html")
 
+
 @app.route('/adicionarCurso', methods=['POST'])
 def add_curso():
-    request_data = json.dumps(request.data.decode("utf-8"))
-    print(request_data)
-    return ''
+    request_data = json.loads(request.data.decode("utf-8"))
+    print("request_data", request_data)
+    for key in request_data:
+        print(request_data[key])
+    return json.dumps(request_data), 200
